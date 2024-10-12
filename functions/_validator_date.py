cea1a759c65578763db2db399c7ed4d293704e59
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-10-12 18:06:47 -0300

    Refatoração da função de validação de datas para a utilização do snack_bar.

--- functions/_validator_date.py
+++ functions/_validator_date.py
@@ -1,11 +1,13 @@
+import flet as ft
 from re import search
 from datetime import date
 from typing import Literal
+from components import custom_snack_bar
 
 
-def validate_date(date_string: str, __format: Literal['BR', 'US', 'ISO'] = 'BR') -> date | bool:
+def validate_date(date_field: ft.TextField, page: ft.Page, __format: Literal['BR', 'US', 'ISO'] = 'BR') -> date | None:
     try:
-        valid = [int(d) for d in search(r'^([0-9]{2,4})[/-]?([0-9]{2})[/-]?([0-9]{2,4})$', date_string).groups()]
+        valid = [int(d) for d in search(r'^([0-9]{2,4})[/-]?([0-9]{2})[/-]?([0-9]{2,4})$', date_field.value).groups()]
         match __format:
             case 'BR':
                 return date(day=valid[0], month=valid[1], year=valid[2])
@@ -14,6 +16,8 @@ def validate_date(date_string: str, __format: Literal['BR', 'US', 'ISO'] = 'BR')
             case 'ISO':
                 return date(day=valid[2], month=valid[1], year=valid[0])
             case _:
-                raise ValueError
-    except (TypeError, ValueError, AttributeError):
-        return False
+                raise ValueError(' Format date not defined!')
+    except (TypeError, ValueError, AttributeError) as error:
+        if isinstance(error, AttributeError):
+            error = 'Value is not valid for a date'
+        custom_snack_bar(validate_date, f'{error}.', page)
