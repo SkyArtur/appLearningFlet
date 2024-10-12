8feaea4036e3540d514b06d9dad944b9f7701569
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-10-12 18:07:01 -0300

    Refatoração da função de validação de e-mail para a utilização do snack_bar.

--- functions/_validator_email.py
+++ functions/_validator_email.py
@@ -1,10 +1,15 @@
+import flet as ft
 from re import search
+from database import dbSQLite
+from components import custom_snack_bar
 
 
-def validate_email(email: str):
+def validate_email(email_field: ft.TextField, page: ft.Page) -> str | None:
     try:
-        valid_email = search(r'^([a-z0-9_]{5,100})@([a-z0-9]{5,100}).([a-z]{2,3})(.)?([a-z]{2})?$', email).string
+        valid_email = search(r'^[a-z0-9_]{1,100}@[a-z0-9]{1,100}\.[a-z]{2,3}[.]?[a-z]{2}?$', email_field.value).string
+        if dbSQLite.fetchone('select id from users where email = ?;', valid_email):
+            raise ValueError('Email already registered!')
         return valid_email
-    except (TypeError, ValueError, AttributeError):
-        return False
+    except (TypeError, ValueError, AttributeError) as error:
+        custom_snack_bar(validate_email, f'{error}.', page)
 
