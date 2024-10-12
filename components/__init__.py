fa606eb7b1208552c5b207dab6035816a769733a
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-10-12 18:06:09 -0300

    Criação do componente snack_bar para exibição de mensagens de erro.

---
+++ components/__init__.py
@@ -0,0 +1,1 @@
+from ._app_snack_bar import custom_snack_bar
---
+++ components/_app_snack_bar.py
@@ -0,0 +1,23 @@
+from typing import Callable
+
+from flet import SnackBar, Text, TextSpan, Page, TextStyle, FontWeight, colors
+
+def custom_snack_bar(function: Callable, message: str, page: Page) -> None:
+    page.overlay.append(
+        SnackBar(
+            content=Text(
+                spans=[
+                    TextSpan(text=f'ERROR <function: {function.__name__}>: ', style=TextStyle(color=colors.RED_400)),
+                    TextSpan(text=message.upper(), style=TextStyle(weight=FontWeight.BOLD))
+                ],
+                color=colors.BLACK,
+            ),
+            bgcolor=colors.AMBER_50,
+            show_close_icon=True,
+            close_icon_color=colors.BLACK,
+            open=True
+        )
+    )
+    page.update()
+
+
