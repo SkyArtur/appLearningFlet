0431c875df9120567cd3247a556d9d2db3577034
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-10-12 22:12:02 -0300

    Criação do componente formulário para cadastro de novo usuário do sistema.

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,4 @@
 from ._app_snack_bar import custom_snack_bar
 from ._app_text_field import custom_text_field, custom_password_field
 from ._app_rows import form_row
+from ._form_register_new_user import form_register_new_user
