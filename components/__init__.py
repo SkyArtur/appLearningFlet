d7f23e0c7157f5a905d10f88d9e0943c2bbff513
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-10-12 22:38:46 -0300

    Nome de arquivos renomeados.

--- components/__init__.py
+++ components/__init__.py
@@ -1,4 +1,4 @@
-from ._app_snack_bar import custom_snack_bar
-from ._app_text_field import custom_text_field, custom_password_field
-from ._app_rows import form_row
-from ._form_register_new_user import form_register_new_user
+from ._snack_bars import custom_snack_bar
+from ._fields import custom_text_field, custom_password_field
+from ._rows import form_row
+from ._forms import form_register_new_user, form_login
